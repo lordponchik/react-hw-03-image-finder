6bd95c87cb41a30c9c19694daf731a6426c06cdf
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-04-08 18:07:33 +0200

    add scroll for images

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,14 +2,32 @@ import PropTypes from 'prop-types';
 
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import s from './ImageGallery.module.css';
-import { Component } from 'react';
+import React, { Component } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 
 export class ImageGallery extends Component {
+  refsImages = React.createRef();
+
+  getSnapshotBeforeUpdate(prevProps, _) {
+    if (prevProps.images.length < this.props.images.length) {
+      const list = this.refsImages.current;
+
+      return list.scrollHeight;
+    }
+    return null;
+  }
+
+  componentDidUpdate(_, __, snapshot) {
+    if (snapshot !== null) {
+      scroll.scrollTo(snapshot);
+    }
+  }
+
   render() {
     const { images, openModal } = this.props;
 
     return (
-      <ul className={s.imageGallery}>
+      <ul className={s.imageGallery} ref={this.refsImages}>
         {images.map((image, id) => {
           return (
             <ImageGalleryItem
